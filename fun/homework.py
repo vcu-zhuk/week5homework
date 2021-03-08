88b73807be8d1b05f5af917c714969f841d86b96
Keni Zhu <zhuk@vcu.edu>
2021-03-08 16:13:10 -0500

    ch

--- fun/homework.py
+++ fun/homework.py
@@ -8,7 +8,7 @@ def find_greatest_number(incoming_list):
     """
     largest_value = max(incoming_list)
     return largest_value
-    
+
 
 
 def find_least_number(incoming_list):
@@ -18,7 +18,7 @@ def find_least_number(incoming_list):
     """
     smallest_value = min(incoming_list)
     return smallest_value
-  
+
 
 
 def add_list_numbers(incoming_list):
@@ -30,7 +30,7 @@ def add_list_numbers(incoming_list):
         return 0
     total_value = sum(incoming_list)
     return total_value
-   
+
 
 
 def longest_value_key(incoming_dict):
