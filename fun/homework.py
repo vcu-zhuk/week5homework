b3f47f9c7b58b40ce7bbb06057d38ce236c58197
Keni Zhu <zhuk@vcu.edu>
2021-03-08 16:09:07 -0500

    ttt

--- fun/homework.py
+++ fun/homework.py
@@ -2,18 +2,30 @@
 
 
 def find_greatest_number(incoming_list):
+    """
+    Required parameter, incoming_list, should be a list.
+    Find the largest number in the list.
+    """
     largest_value = max(incoming_list)
     return largest_value
     
 
 
 def find_least_number(incoming_list):
+    """
+    Required parameter, incoming_list, should be a list.
+    Find the smallest/least number in the list.
+    """
     smallest_value = min(incoming_list)
     return smallest_value
   
 
 
 def add_list_numbers(incoming_list):
+    """
+    Required parameter, incoming_list, should be a list.
+    Add all the values together and return it.
+    """
     if incoming_list is None:
         return 0
     total_value = sum(incoming_list)
@@ -22,6 +34,10 @@ def add_list_numbers(incoming_list):
 
 
 def longest_value_key(incoming_dict):
+    """
+    Required parameter, incoming_list, should be a list.
+    Add all the values together and return it.
+    """
     if incoming_dict is None:
         return None
     highest_length_key = max(incoming_dict, key=lambda k: len(incoming_dict[k]))
