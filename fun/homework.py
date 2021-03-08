da387ce6e7693e3f298bc891e0b12b9d33b392b1
Keni Zhu <zhuk@vcu.edu>
2021-03-08 15:44:36 -0500

    try

--- fun/homework.py
+++ fun/homework.py
@@ -2,45 +2,27 @@
 
 
 def find_greatest_number(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Find the largest number in the list.
-    """
-    return_value = max(incoming_list)
-    return return_value
+    return max(incoming_list)
+    
 
 
 def find_least_number(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Find the smallest/least number in the list.
-    """
-    return_value = min(incoming_list)
-    return return_value
+     return min(incoming_list)
+  
 
 
 def add_list_numbers(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Add all the values together and return it.
-    """
     if incoming_list is None:
         return 0
-    return_value = sum(incoming_list)
-    return return_value
+    return sum(incoming_list)
    
 
 
 def longest_value_key(incoming_dict):
-    """
-    Required parameter, incoming_dict, should be a dict.
-    Find the KEY that has a value with the highest length, use the len() function
-    """
-    if incoming_dict is not None:
-        if len(incoming_dict) == 0:
-            return None
-        longest_key = max(incoming_dict, key=lambda x: len(incoming_dict[x]))
-        return longest_key
-    else:
+    if incoming_dict is None:
         return None
-    
+    else:
+        len(incoming_dict) == 0
+        return None
+    highest_length_key = max(incoming_dict, key=lambda k: len(incoming_dict[k]))
+    return highest_length_key
