5c16b6a6d7277ea2cd008bdfd6bf2e67d1ff9f2c
Keni Zhu <zhuk@vcu.edu>
2021-03-08 16:01:42 -0500

    noo

--- fun/homework.py
+++ fun/homework.py
@@ -2,19 +2,22 @@
 
 
 def find_greatest_number(incoming_list):
-    return max(incoming_list)
+    largest_value = max(incoming_list)
+    return largest_value
     
 
 
 def find_least_number(incoming_list):
-     return min(incoming_list)
+    smallest_value = min(incoming_list)
+    return smallest_value
   
 
 
 def add_list_numbers(incoming_list):
     if incoming_list is None:
         return 0
-    return sum(incoming_list)
+    total_value = sum(incoming_list)
+    return total_value
    
 
 
