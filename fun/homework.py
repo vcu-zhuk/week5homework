9d11bf625c67903fdd04ddf95866256e91d48ea6
Keni Zhu <zhuk@vcu.edu>
2021-03-08 17:26:30 -0500

    try

--- fun/homework.py
+++ fun/homework.py
@@ -38,7 +38,14 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    if incoming_dict is None:
-        return None
-    highest_length_key = max(len(x) for x in incoming_dict)
-    return highest_length_key
+    try:
+        key_result = None
+        max_value = 0
+        for key in incoming_dict:
+            if len(incoming_dict[key]) > max_value:
+                max_value = len(incoming_dict[key])
+                key_result = key
+    except:
+        incoming_dict = None
+
+    return key_result
