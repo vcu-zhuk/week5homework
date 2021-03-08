81dfd30999bbccefcf5a4c6e7dd5ec220aec4abd
Keni Zhu <zhuk@vcu.edu>
2021-03-08 04:36:09 -0500

    just try

--- fun/homework.py
+++ fun/homework.py
@@ -6,15 +6,19 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    pass
+    assert incoming_list == [1, 2, 3, 4, 5, 6, 7, 8]
+    return_value = max(incoming_list)
+    return return_value
 
 
 def find_least_number(incoming_list):
     """
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
-    """
-    pass
+    """ 
+    assert incoming_list == [1, 2, 3, 4, 5, 6, 7, 8]
+    return_value = min(incoming_list)
+    return return_value
 
 
 def add_list_numbers(incoming_list):
@@ -22,8 +26,10 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-
-    pass
+    assert incoming_list == [1, 2, 3, 4, 5, 6, 7, 8]
+    return_value = max(incoming_list)
+    return return_value
+  
 
 
 def longest_value_key(incoming_dict):
@@ -31,4 +37,6 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    pass
+    assert incoming_list == [1, 2, 3, 4, 5, 6, 7, 8]
+    return_value = max(incoming_list)
+    return return_value
