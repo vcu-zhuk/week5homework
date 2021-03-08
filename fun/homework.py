4c28d25b8f46b1e7bce2ad06b57daae60c7fbaaf
Keni Zhu <zhuk@vcu.edu>
2021-03-08 04:42:31 -0500

    ttt

--- fun/homework.py
+++ fun/homework.py
@@ -6,9 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    assert incoming_list == [1, 2, 3, 4, 5, 6, 7, 8]
-    return_value = max(incoming_list)
-    return return_valu
+    
 
 
 def find_least_number(incoming_list):
@@ -16,9 +14,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """ 
-    assert incoming_list == [1, 2, 3, 4, 5, 6, 7, 8]
-    return_value = min(incoming_list)
-    return return_value
+  
 
 
 def add_list_numbers(incoming_list):
@@ -26,10 +22,7 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-    assert incoming_list == [1, 2, 3, 4, 5, 6, 7, 8]
-    return_value = max(incoming_list)
-    return return_value
-  
+   
 
 
 def longest_value_key(incoming_dict):
